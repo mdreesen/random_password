10451bb2b55997a5670bc700448e8e88f6031250
Michael Dreesen <michaeldreesen90@gmail.com>
2020-08-20 22:12:38 -0600

    changing up code for the password generator

--- script.js
+++ script.js
@@ -1,62 +1,31 @@
-const characterAmountRange = document.getElementById('characterAmountRange')
-const characterAmountNumber = document.getElementById('characterAmountNumber')
-const includeUppercaseElement = document.getElementById('includeUppercase')
-const includeNumbersElement = document.getElementById('includeNumbers')
-const includeSymbolsElement = document.getElementById('includeSymbols')
-const form = document.getElementById('passwordGeneratorForm')
-const passwordDisplay = document.getElementById('passwordDisplay')
+// creating arrays for each password use case
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '.', ',', '?', '√', '©', '∆', '˚', 'π', '®'];
 
-const lowToHigh = arrayFromLowToHigh(8, 128)
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// This is the lowercase, uppercase, and symbol characters
-var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ];
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    passwordText.value = password;
+    console.log(writePassword);
 
-var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
-var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '.', ',', '?', '√', '©', '∆', '˚', 'π', '®'];
-
-
-
-// This syncs up the number input and slider
-characterAmountNumber.addEventListener('input', syncCharacterAmount)
-characterAmountRange.addEventListener('input', syncCharacterAmount)
-
-function syncCharacterAmount(e) {
-    const value = e.target.value
-    characterAmountNumber.value = value
-    characterAmountRange.value = value
 }
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    const characterAmount = characterAmountNumber.value
-    const includeUppercase = includeUppercaseElement.checked
-    const includeNumbers = includeNumbersElement.checked
-    const includeSymbols = includeSymbolsElement.checked
-    const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
-    passwordDisplay.innerText = password
-})
-
-function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
-    let characters = lowercase
-    if (includeUppercase) characters = characters.concat(uppercase)
-    if (includeNumbers) characters = characters.concat(numbers)
-    if (includeSymbols) characters = characters.concat(symbols)
-
-    const passwordCharacters = []
-    for (let i = 0; i < characterAmount; i++) {
-        const characterCode = characters[Math.floor(Math.random() * characters.length)]
-        passwordCharacters.push(characterCode)
+function generatePassword() {
+    var choose = prompt("Please choose LowerCase (L), UpperCase (U), Numbers (N), or Symbols (S)")
+    console.log(lowerCase);
+    if (choose === "L" || "l") {
+        for (var i = 0; i < lowerCase.length; i++) {
+            writePassword();
+        }
     }
-    return passwordCharacters.join('')
 }
 
-function arrayFromLowToHigh(low, high) {
-    const array = []
-    for (var i = low; i <= high; i++) {
-        array.push(i)
-    }
-    return array
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
